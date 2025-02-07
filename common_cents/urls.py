6565d6b53606850a22f0f509f22cbdb4e4c343e1
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2025-02-07 12:09:49 +0000

    Base app + some styling

--- common_cents/urls.py
+++ common_cents/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('webapp.urls')),
     path('calculators/mortgage/', include('mortgage_calculator.urls')),
 ]
--- mortgage_calculator/urls.py
+++ mortgage_calculator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'calculators'
+
 urlpatterns = [
     path('affordability', views.AffordabilityCalculatorView.as_view(), name='affordability_calculator'),
     path('overpayment', views.OverpaymentCalculatorView.as_view(), name='overpayment_calculator'),
